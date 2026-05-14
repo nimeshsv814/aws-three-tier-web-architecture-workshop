7d3a4e416be6da8791d24308d3bcef7dabf633d7
Nimesh Sundaram Veeramani(UST <307365@ust.com>
2026-05-14 12:14:13 +0530

    Check

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -1,6 +1,6 @@
 module.exports = Object.freeze({
     DB_HOST : '10.0.13.138',
-    DB_USER : 'admin',
+    DB_USER : 'appuser',
     DB_PWD : 'YourStrongPassword123!',
     DB_DATABASE : 'webappdb'
 });
